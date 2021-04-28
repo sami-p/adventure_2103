e47b34d039faf445066851a9601139d27f95c40d
sami-p <sami.peterson14@gmail.com>
2021-04-28 10:06:18 -0500

    Add Trail, Hiker, Park spec files

---
+++ spec/trail_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+require './lib/trail'
+require './lib/hiker'
+require './lib/park'
+
+RSpec.describe Trail do
+  describe '#initialize' do
+    it 'exists' do
+      trail1 = Trail.new({name: 'Grand Wash', length: '2.2 miles', level: :easy})
+      expect(trail1).to be_an_instance_of(Trail)
+    end
+  end
+end
