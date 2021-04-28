3236bd570e0c95bd18e9827c44721f4c77c72485
sami-p <sami.peterson14@gmail.com>
2021-04-28 11:41:23 -0500

    Iteration 2, trails 1 - 6 for parks 1 & 2

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -47,4 +47,34 @@ RSpec.describe Park do
       expect(park2.trails).to eq([trail3])
     end
   end
+
+  describe 'Iteration 2' do
+    it 'adds trails to park1' do
+      park1 = Park.new('Capitol Reef')
+
+      trail1 = Trail.new({name: 'Grand Wash', length: '2.2 miles', level: :easy})
+      trail2 = Trail.new({name: 'Cohab Canyon', length: '1.7 miles', level: :moderate})
+      trail3 = Trail.new({name: 'Chimney Rock Loop', length: '3.6 miles', level: :strenuous})
+
+      park1.add_trail(trail1)
+      park1.add_trail(trail2)
+      park1.add_trail(trail3)
+
+      expect(park1.trails).to eq([trail1, trail2, trail3])
+    end
+
+    it 'adds trails to park 2' do
+      park2 = Park.new('Bryce Canyon')
+
+      trail4 = Trail.new({name: "Queen's/Navajo Loop", length: '2.9 miles', level: :moderate})
+      trail5 = Trail.new({name: 'Rim Trail', length: '11 miles', level: :easy})
+      trail6 = Trail.new({name: 'Tower Bridge', length: '3 miles', level: :moderate})
+      
+      park2.add_trail(trail4)
+      park2.add_trail(trail5)
+      park2.add_trail(trail6)
+
+      expect(park2.trails).to eq([trail4, trail5, trail6])
+    end
+  end
 end
