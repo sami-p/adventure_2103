095cf386037bf050c03774ae15f0b1237b60b7ea
sami-p <sami.peterson14@gmail.com>
2021-04-28 10:29:17 -0500

    Test #add_trails

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -20,4 +20,17 @@ RSpec.describe Park do
       expect(park1.trails).to eq([])
     end
   end
+
+  describe '#add_trail' do
+    it 'can add a trail' do
+      park1 = Park.new('Capitol Reef')
+      trail1 = Trail.new({name: 'Grand Wash', length: '2.2 miles', level: :easy})
+      trail2 = Trail.new({name: 'Cohab Canyon', length: '1.7 miles', level: :moderate})
+
+      park1.add_trail(trail1)
+      park1.add_trail(trail2)
+
+      expect(park1.trails).to eq([trail1, trail2])
+    end
+  end
 end
